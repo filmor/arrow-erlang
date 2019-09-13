563266f1f948a805f85aa416262ff2aea9e59e00
Benedikt Reinartz <filmor@gmail.com>
2019-09-13 11:19:23 +0200

    Implement data type conversion

--- crates/arrow_nif/src/lib.rs
+++ crates/arrow_nif/src/lib.rs
@@ -1,20 +1,184 @@
-use rustler::{Encoder, Env, Error, Term};
+use arrow::datatypes::{DataType, DateUnit, Field, IntervalUnit, TimeUnit};
+use arrow::array::{ ArrayData };
+use rustler::types::{Atom, ListIterator};
+use rustler::{Encoder, Env, Error, NifResult, Term};
 
 mod atoms {
     rustler::rustler_atoms! {
         atom ok;
+
+        atom boolean;
+        atom int;
+        atom uint;
+        atom float;
+        atom timestamp;
+        atom date;
+        atom time;
+        atom interval;
+        atom utf8;
+        atom list;
+
+        atom s;
+        atom ms;
+        atom us;
+        atom ns;
+
+        atom d;
+
+        atom year_month;
+        atom day_time;
     }
+}
+
+mod data_types {
+    rustler::rustler_atoms! {}
 }
 
 rustler::rustler_export_nifs! {
     "arrow",
     [
-        ("test", 0, test)
+        ("new_array", 2, new_array)
     ],
     None
 }
 
+fn new_array<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    let data_type = decode_type(args[0])?;
 
-fn test<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
+    let array = ArrayData::builder(data_type)
+        .len(args[1].decode()?)
+        .build();
+
+    println!("Created array {:?}", array);
+    
     Ok(atoms::ok().encode(env))
+}
+
+fn decode_type<'a>(term: Term<'a>) -> Result<DataType, Error> {
+    if term.is_atom() {}
+    if term.is_tuple() {
+        if let Ok((name, modifier)) = term.decode::<(Atom, Term<'a>)>() {
+            if name == atoms::int() {
+                let length = modifier.decode()?;
+
+                match length {
+                    8 => return Ok(DataType::Int8),
+                    16 => return Ok(DataType::Int16),
+                    32 => return Ok(DataType::Int32),
+                    64 => return Ok(DataType::Int64),
+                    _ => return Err(Error::BadArg),
+                }
+            }
+            if name == atoms::uint() {
+                let length = modifier.decode()?;
+
+                match length {
+                    8 => return Ok(DataType::UInt8),
+                    16 => return Ok(DataType::UInt16),
+                    32 => return Ok(DataType::UInt32),
+                    64 => return Ok(DataType::UInt64),
+                    _ => return Err(Error::BadArg),
+                }
+            }
+            if name == atoms::float() {
+                let length = modifier.decode()?;
+
+                match length {
+                    16 => return Ok(DataType::Float16),
+                    32 => return Ok(DataType::Float32),
+                    64 => return Ok(DataType::Float64),
+                    _ => return Err(Error::BadArg),
+                }
+            }
+
+            if name == atoms::list() {
+                let sub_type = decode_type(modifier)?;
+                return Ok(DataType::List(Box::new(sub_type)));
+            }
+
+            if name == atoms::timestamp() {
+                let unit = modifier.decode::<Atom>()?;
+                let unit = if unit == atoms::s() {
+                    TimeUnit::Second
+                } else if unit == atoms::ms() {
+                    TimeUnit::Millisecond
+                } else if unit == atoms::us() {
+                    TimeUnit::Microsecond
+                } else if unit == atoms::ns() {
+                    TimeUnit::Nanosecond
+                } else {
+                    return Err(Error::BadArg);
+                };
+
+                return Ok(DataType::Timestamp(unit))
+            }
+
+            if name == atoms::interval() {
+                let unit = modifier.decode::<Atom>()?;
+                if unit == atoms::year_month() {
+                    return Ok(DataType::Interval(IntervalUnit::YearMonth));
+                }
+                if unit == atoms::day_time() {
+                    return Ok(DataType::Interval(IntervalUnit::DayTime));
+                }
+
+                return Err(Error::BadArg);
+            }
+        }
+
+        if let Ok((name, size, unit)) = term.decode::<(Atom, i32, Atom)>() {
+            if name == atoms::date() {
+                let unit = if unit == atoms::d() {
+                    DateUnit::Day
+                } else if unit == atoms::ms() {
+                    DateUnit::Millisecond
+                } else {
+                    return Err(Error::BadArg);
+                };
+
+                match size {
+                    32 => return Ok(DataType::Date32(unit)),
+                    64 => return Ok(DataType::Date64(unit)),
+                    _ => return Err(Error::BadArg),
+                }
+            }
+
+            if name == atoms::time() {
+                let unit = if unit == atoms::s() {
+                    TimeUnit::Second
+                } else if unit == atoms::ms() {
+                    TimeUnit::Millisecond
+                } else if unit == atoms::us() {
+                    TimeUnit::Microsecond
+                } else if unit == atoms::ns() {
+                    TimeUnit::Nanosecond
+                } else {
+                    return Err(Error::BadArg);
+                };
+
+                match size {
+                    32 => return Ok(DataType::Time32(unit)),
+                    64 => return Ok(DataType::Time64(unit)),
+                    _ => return Err(Error::BadArg),
+                }
+            }
+
+        }
+    }
+    if term.is_list() {
+        let iter: ListIterator = term.decode()?;
+
+        let mut fields = vec![];
+
+        for t in iter {
+            let (name, field_type): (Atom, Term<'a>) = t.decode()?;
+            let field_type = decode_type(field_type)?;
+            let field = Field::new(format!("{:?}", name).as_str(), field_type, false);
+            fields.push(field);
+        }
+
+        return Ok(DataType::Struct(fields));
+    }
+
+    Err(Error::BadArg)
 }
--- src/arrow.erl
+++ src/arrow.erl
@@ -1,8 +1,32 @@
 -module(arrow).
 
 -export([
-    test/0
+    new_array/2
 ]).
+
+-export_type([
+    type/0,
+    data_type/0
+]).
+
+-type time_unit() :: s | ms | us | ns.
+-type date_unit() :: d | ms.
+-type interval_unit() :: year_month | day_time.
+-type data_type() ::
+      boolean
+    | utf8
+    | {int, 8 | 16 | 32 | 64}
+    | {uint, 8 | 16 | 32 | 64}
+    | {float, 32 | 64}
+    | {date, date_unit()}
+    | {time, time_unit()}
+    | {timestamp, time_unit()}
+    | {interval, interval_unit()}
+    | {list, data_type()}
+    | [{atom(), data_type()}]
+    .
+
+-opaque type() :: reference().
 
 -on_load(init/0).
 
@@ -10,5 +34,6 @@ init() ->
     {ok, Lib} = find_crate:find_library(arrow, "arrow_nif"),
     erlang:load_nif(Lib, 0).
 
-
-test() -> error.
+-spec new_array(Type :: data_type(), Length :: non_neg_integer()) -> {ok, type()} | {error, _}.
+new_array(_Type, _Length) ->
+    erlang:nif_error(nif_not_loaded).
